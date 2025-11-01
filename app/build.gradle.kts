fc407f5378680f71e5f68586cdfd39c17662b508
MM20 <15646950+MM2-0@users.noreply.github.com>
2025-11-01 13:56:57 +0100

    Version 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "de.mm20.launcher2.plugin.foursquare"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
